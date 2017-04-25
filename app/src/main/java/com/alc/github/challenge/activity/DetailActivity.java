732c088abaf588e3ebf20258be7dba1fc544e143
Gino <ginowine1@gmail.com>
2017-04-25 17:05:21 +0100

    url opens gitHub profile

--- app/src/main/java/com/alc/github/challenge/activity/DetailActivity.java
+++ app/src/main/java/com/alc/github/challenge/activity/DetailActivity.java
@@ -1,17 +1,25 @@
 package com.alc.github.challenge.activity;
 
 import android.content.Context;
+import android.net.Uri;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
 import com.alc.github.challenge.R;
+import com.alc.github.challenge.util.Share;
 import com.squareup.picasso.Picasso;
 
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.text.util.Linkify;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import com.alc.github.challenge.model.GitUser;
+import me.saket.bettermovementmethod.BetterLinkMovementMethod;
+import android.support.customtabs.CustomTabsIntent;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -35,6 +43,14 @@ public class DetailActivity extends AppCompatActivity {
         mUsername = (TextView) findViewById(R.id.username);
         mProfileUrl = (TextView) findViewById(R.id.profile_url);
         mUserImage = (ImageView) findViewById(R.id.image);
+
+        Button shareButton = (Button) findViewById(R.id.share_button);
+        shareButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                shareUserProfile();
+            }
+        });
 
         CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
 
@@ -60,5 +76,29 @@ public class DetailActivity extends AppCompatActivity {
                 .placeholder(android.R.drawable.sym_def_app_icon)
                 .error(android.R.drawable.sym_def_app_icon)
                 .into(mUserImage);
+
+        BetterLinkMovementMethod movementMethod = BetterLinkMovementMethod.linkify(Linkify.WEB_URLS, mProfileUrl);
+        movementMethod.setOnLinkClickListener(new BetterLinkMovementMethod.OnLinkClickListener(){
+            @Override
+            public boolean onClick(TextView textView, String url) {
+                getCustomTabIntentInstance().launchUrl(DetailActivity.this, Uri.parse(url));
+                return true;
+            }
+        });
+    }
+
+    private CustomTabsIntent getCustomTabIntentInstance() {
+        CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+        builder.setToolbarColor(ContextCompat.getColor(this, R.color.colorPrimary));
+        return builder.build();
+    }
+
+
+    public void shareUserProfile(){
+        GitUser user = getIntent().getParcelableExtra("user");
+
+        String message = "Check out this awesome developer @" + user.getLogin() + ", " + user.getUrl();
+        Share.shareCustom(message, context);
+
     }
 }
